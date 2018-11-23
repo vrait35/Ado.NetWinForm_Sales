c1db4224bfedc34cc702ebda3bb8422a7502c5f0
vrait35 <vrait35@mail.ru>
2018-11-24 03:58:00 +0600

    asd

---
+++ Ado.NetWinFormSdelki/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Ado.NetWinFormSdelki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(130, 127);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(482, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(336, 170);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 500);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Ado.NetWinFormSdelki/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ado.NetWinFormSdelki
+{
+    public partial class Form1 : Form
+    {
+        string connectSqlServer = ConfigurationManager.ConnectionStrings["vrait34"].ConnectionString;
+        string connectionString = ConfigurationManager.ConnectionStrings["vrait35"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = "CREATE DATABASE MyDatabase142";
+            
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectSqlServer);
+            using (SqlConnection myConn = new SqlConnection(builder.ConnectionString))
+            {
+                SqlCommand myCommand = new SqlCommand(sqlQuery, myConn);
+                try
+                {
+                    myConn.Open();
+                    myCommand.ExecuteNonQuery();
+                    MessageBox.Show("БД создалась");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show(" бд существует ");
+                }
+
+                finally
+                {
+                    myConn.Close();
+                }
+            }
+
+            
+            builder.ConnectionString = connectionString;
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                SqlCommand sql = new SqlCommand();
+                sql.Connection = connection;
+                try
+                {                  
+                    sql.CommandText = "create table Clients(id_client int primary key,last_name varchar(50),first_name varchar(50))";
+                    sql.ExecuteNonQuery();
+
+                    sql.CommandText = "create table Sallers(id_saller int primary key,last_name varchar(50),first_name varchar(50))";
+                    sql.ExecuteNonQuery();
+
+                    sql.CommandText = "create table Sales(id_sale int primary key,id_client int foreign key references Clients(id_client)," +
+                        "id_saller int foreign key references Sallers(id_saller),price  int)";
+                    sql.ExecuteNonQuery();
+
+                    sql.CommandText = "insert into Clients values(1,'федоров','федя')";
+                    sql.ExecuteNonQuery();
+                    sql.CommandText = "insert into Clients values(2,'сашов','саша')";
+                    sql.ExecuteNonQuery();
+
+                    sql.CommandText = "insert into Sallers values(1,'юлива','юля')";
+                    sql.ExecuteNonQuery();
+                    sql.CommandText = "insert into Sallers values(2,'наташова','наташа')";
+                    sql.ExecuteNonQuery();
+
+                    sql.CommandText = "insert into Sales values(1,1,2,80)";
+                    sql.ExecuteNonQuery();
+                    sql.CommandText = "insert into Sales values(2,2,1,50)";
+                    sql.ExecuteNonQuery();
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            //combobox1
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                SqlCommand sql = new SqlCommand();
+                try
+                {
+                 //   sql.CommandText = "select*from Sales";
+                 //   SqlDataReader reader=  sql.ExecuteReader();
+                 //   while (reader.Read())
+                 //   {
+                 //   }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    comboBox1.Items.Add("Клиенты");
+                    comboBox1.Items.Add("Продавцы");
+                    comboBox1.Items.Add("Продажи");
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            int numTable = -1;
+            try
+            {               
+                numTable = comboBox1.SelectedIndex;
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //MessageBox.Show(numTable.ToString());
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectSqlServer);
+            builder.ConnectionString = connectionString;
+            
+            switch (numTable)
+            {
+                case 0:
+                    {
+                        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                        {
+                            connection.Open();
+                            SqlCommand sql = new SqlCommand();
+                            sql.Connection = connection;
+                            try
+                            {
+                                sql.CommandText = "select*from Clients";
+                                SqlDataReader reader = sql.ExecuteReader();
+                                //dataGridView1.ColumnCount = 3;
+                                //dataGridView1.Columns.Add("id_client", "id_saller", "price");
+                                dataGridView1.ColumnCount = 3;
+                                dataGridView1.Columns.Add("id_client", "id_client");
+                                dataGridView1.Columns.Add("id_saller", "id_saller");
+                                dataGridView1.Columns.Add("price", "price");
+                                dataGridView1.Rows.Add("id_client", "last_name", "first_name");
+                                // dataGridView1.Rows.Add("asd","x","");
+                                // dataGridView1.Rows.Add("asd","x2");
+                                // dataGridView1.Rows.Add("asd","x1");
+                                while (reader.Read())
+                                {
+                                    //dataGridView1.Columns.Add("id_client", reader["id_client"].ToString());
+                                    dataGridView1.Rows.Add(reader["id_client"].ToString(), reader["last_name"].ToString(), reader["first_name"].ToString());
+                                }
+                            }
+                            catch (SqlException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (InvalidOperationException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            finally
+                            {
+                                connection.Close();
+                            }
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                        {
+                            connection.Open();
+                            SqlCommand sql = new SqlCommand();
+                            sql.Connection = connection;
+                            try
+                            {
+                                sql.CommandText = "select*from Sallers";
+                                SqlDataReader reader = sql.ExecuteReader();
+                                //dataGridView1.ColumnCount = 3;
+                                //dataGridView1.Columns.Add("id_client", "id_saller", "price");
+                                dataGridView1.ColumnCount = 3;
+                                dataGridView1.Columns.Add("id_client", "id_client");
+                                dataGridView1.Columns.Add("id_saller", "id_saller");
+                                dataGridView1.Columns.Add("price", "price");
+                                dataGridView1.Rows.Add("id_saller", "last_name", "first_name");
+                                // dataGridView1.Rows.Add("asd","x","");
+                                // dataGridView1.Rows.Add("asd","x2");
+                                // dataGridView1.Rows.Add("asd","x1");
+                                while (reader.Read())
+                                {
+                                    //dataGridView1.Columns.Add("id_client", reader["id_client"].ToString());
+                                    dataGridView1.Rows.Add(reader["id_saller"].ToString(), reader["last_name"].ToString(), reader["first_name"].ToString());
+                                }
+                            }
+                            catch (SqlException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (InvalidOperationException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            finally
+                            {
+                                connection.Close();
+                            }
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                        {
+                            connection.Open();
+                            SqlCommand sql = new SqlCommand();
+                            sql.Connection = connection;
+                            try
+                            {
+                                sql.CommandText = "select*from Sales";
+                                SqlDataReader reader = sql.ExecuteReader();
+                                //dataGridView1.ColumnCount = 3;
+                                //dataGridView1.Columns.Add("id_client", "id_saller", "price");
+                                dataGridView1.ColumnCount = 3;
+                                dataGridView1.Columns.Add("id_client", "id_client");
+                                dataGridView1.Columns.Add("id_saller", "id_saller");
+                                dataGridView1.Columns.Add("price", "price");
+                                dataGridView1.Rows.Add("id_client", "id_saller", "price");
+                                // dataGridView1.Rows.Add("asd","x","");
+                                // dataGridView1.Rows.Add("asd","x2");
+                                // dataGridView1.Rows.Add("asd","x1");
+                                while (reader.Read())
+                                {
+                                    //dataGridView1.Columns.Add("id_client", reader["id_client"].ToString());
+                                    dataGridView1.Rows.Add(reader["id_client"].ToString(), reader["id_saller"].ToString(), reader["price"].ToString());
+                                }
+                            }
+                            catch (SqlException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (InvalidOperationException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            finally
+                            {
+                                connection.Close();
+                            }
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
